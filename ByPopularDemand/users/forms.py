0267a941ed05ba3b9ba76bbef4766ddc97c5e797
Riaan Theron <kawasakiriaan@gmail.com>
2023-04-06 14:22:27 +0200

    Added docstrings to ByPopularDemand/users/forms.py

--- ByPopularDemand/users/forms.py
+++ ByPopularDemand/users/forms.py
@@ -4,11 +4,22 @@ from django.contrib.auth.forms import UserCreationForm
 
 
 class LoginForm(forms.Form):
+    """A form for user login.
+
+    Allows users to enter their username and password.
+    """
+
     username = forms.CharField(max_length=65)
     password = forms.CharField(max_length=65, widget=forms.PasswordInput)
 
 
 class RegisterForm(UserCreationForm):
+    """A form for user registration.
+
+    Allows users to create an account by entering their username, email,
+    and password.
+    """
+    
     first_name = forms.CharField(max_length=65)
     class Meta:
         model = User
